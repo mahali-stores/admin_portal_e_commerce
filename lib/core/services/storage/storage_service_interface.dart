3e4d1c5c94c1be65ce91be92ed69c630ebaab2ac
aaa <moh>
2025-07-12 18:06:11 +0300

    Deploy Flutter Web to GitHub Pages

--- lib/core/services/storage/storage_service_interface.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:typed_data';
-
-abstract class IStorageService {
-  Future<String?> uploadImage({
-    required String path,
-    required Uint8List imageData,
-    required String fileName,
-  });
-}
